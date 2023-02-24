edd1851ea114cf96c6b2d22a90b4fedc4af74ad6
Eshwar <eshwarrachakonda02@gmail.com>
2023-02-25 04:36:38 +0530

    4 snippets of python

---
+++ Python/Area.py
@@ -0,0 +1,20 @@
+ 
+#Taking length and breadth as input
+l,b = map(float,input().split(' '))
+
+#Calculating area
+area = l * b
+
+#print the area
+print(int(area))
+
+
+
+
+
+
+
+
+
+
+ 
---
+++ Python/swap.py
@@ -0,0 +1,11 @@
+
+def swap(a, b):
+   
+    temp = 0
+    temp =a
+    a =b
+    b = temp
+    print(a,b,sep=' ')
+
+x,y = input().split()
+swap(x,y)
