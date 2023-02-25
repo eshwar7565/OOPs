51ef1e4de011355489dc6ad9a982a3d6dac84d46
Eshwar <eshwarrachakonda02@gmail.com>
2023-02-25 05:44:52 +0530

    DSA arrays practice

---
+++ LoveBabbarDSAsheet/moveallnegative.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void rearrange(int arr[], int n)
+{
+	int j = 0;
+	for (int i = 0; i < n; i++) {
+		if (arr[i] < 0) {
+			if (i != j)
+				swap(arr[i], arr[j]);
+			j++;
+		}
+	}
+}
+
+// A utility function to print an array
+void printArray(int arr[], int n)
+{
+	for (int i = 0; i < n; i++)
+		printf("%d ", arr[i]);
+}
+
+// Driver code
+int main()
+{
+	int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+	rearrange(arr, n);
+	printArray(arr, n);
+	return 0;
+}
